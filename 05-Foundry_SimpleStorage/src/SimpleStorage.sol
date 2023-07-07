3a0ceea8c1a6ca983ec892b24a5dc67ebad67695
MarsIfeanyi <marschalice@gmail.com>
2023-07-07 13:47:49 +0100

    Create SimpleStorage Contract

---
+++ 05-Foundry_SimpleStorage/src/SimpleStorage.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+// pragma solidity ^0.8.0;
+// pragma solidity >=0.8.0 <0.9.0;
+
+contract SimpleStorage {
+    // Storage/State Varaible... Stored on the blockchain
+    uint256 myFavoriteNumber;
+
+    // Struct... Custom type (User defined type)
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    Person public mars = Person({favoriteNumber: 20145, name: "Mars"});
+
+    // uint256[] public anArray;
+    Person[] public listOfPeople; // dynamic array
+
+    // Maps someones name to FavoriteNumber
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    // function that stores and updates myFavoriteNumber.... Hint: Everytime you updates the state of a blockchain, is going to cost gas.
+    function store(uint256 _favoriteNumber) public {
+        myFavoriteNumber = _favoriteNumber;
+    }
+
+    // function that allows user to retrieve myFavoriteNumber ie Reading (view) from the state.
+    function retrieve() public view returns (uint256) {
+        return myFavoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        // Person memory newPerson = Person(_favoriteNumber, _name);
+        // listOfPeople.push(newPerson);
+
+        // Adding _favoriteNumber and _name to the array
+        listOfPeople.push(Person(_favoriteNumber, _name));
+
+        // Updating the mapping
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+}
