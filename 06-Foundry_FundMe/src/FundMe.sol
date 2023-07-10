699801f3215a9326bbd9c4f1eabc4dd42cf621ee
MarsIfeanyi <marschalice@gmail.com>
2023-07-10 22:47:09 +0100

    Initial Commit

---
+++ 06-Foundry_FundMe/src/FundMe.sol
@@ -0,0 +1,95 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
+
+error FundMe__NotOwner(); // Hint: Nice convention for naming your error errors is to add the name of the contract and __, This will help to easily know the contract where the error is coming from
+
+contract FundMe {
+    using PriceConverter for uint256;
+
+    // Hint: storage variables should start with s_variableName... VIP private variables are more gas efficient than public variables, thus we want to default all storage variables as private.
+
+    mapping(address => uint256) private s_addressToAmountFunded;
+
+    // Hint: You can also name your mapping
+    //mapping(address amount => uint256 amountFunded) public s_addressToAmountFunded;
+    address[] private s_funders;
+
+    // Could we make this constant?  /* hint: no! We should make it immutable! */
+    address private immutable i_owner;
+    uint256 public constant MINIMUM_USD = 5 * 10 ** 18; // 5 * 1e18
+
+    AggregatorV3Interface private s_priceFeed;
+
+    constructor(address priceFeed) {
+        i_owner = msg.sender;
+        s_priceFeed = AggregatorV3Interface(priceFeed);
+    }
+
+    function fund() public payable {
+        require(
+            msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD,
+            "You need to spend more ETH!"
+        );
+
+        s_addressToAmountFunded[msg.sender] += msg.value;
+        s_funders.push(msg.sender);
+    }
+
+    function getVersion() public view returns (uint256) {
+        return s_priceFeed.version();
+    }
+
+    modifier onlyOwner() {
+        if (msg.sender != i_owner) revert FundMe__NotOwner();
+        _;
+    }
+
+    function withdraw() public onlyOwner {
+        for (
+            /*Starting index, Ending index, Step amount*/
+            uint256 funderIndex = 0;
+            funderIndex < s_funders.length;
+            funderIndex++
+        ) {
+            address funder = s_funders[funderIndex]; // accessing the indexes in the s_funders array
+            s_addressToAmountFunded[funder] = 0; // resetting the mapping.ie we reset everything back to zero after withdrawing.
+        }
+
+        s_funders = new address[](0); // resetting the array
+
+        // call... Hint: Call forwards all gas or set gas, reurns bool
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(callSuccess, "Call failed");
+    }
+
+    fallback() external payable {
+        fund();
+    }
+
+    receive() external payable {
+        fund();
+    }
+
+    /**
+     * View / Pure functions (Getters)
+     */
+
+    function getAddressToAmountFunded(
+        address fundingAddress
+    ) external view returns (uint256) {
+        return s_addressToAmountFunded[fundingAddress];
+    }
+
+    function getFunder(uint256 index) external view returns (address) {
+        return s_funders[index];
+    }
+
+    function getOwner() external view returns (address) {
+        return i_owner;
+    }
+}
