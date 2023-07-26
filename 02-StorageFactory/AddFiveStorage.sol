a96d11f63c6d3ee869c6073f10fe5d51ac556e5f
MarsIfeanyi <marschalice@gmail.com>
2023-07-26 22:26:57 +0100

    Update file

--- 02-StorageFactory/AddFiveStorage.sol
+++ 02-StorageFactory/AddFiveStorage.sol
@@ -6,6 +6,7 @@ import {SimpleStorage} from "./SimpleStorage.sol";
 
 // Contract Inheritance
 contract AddFiveStorage is SimpleStorage {
+    //Hint: overriding the  "store" function defined in the SimpleStorage Contract... myFavoriteNumber is a state variable defined in the SimpleStorage contract.
     function store(uint256 _favoriteNumber) public override {
         myFavoriteNumber = _favoriteNumber + 5;
     }
