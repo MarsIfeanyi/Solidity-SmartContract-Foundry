94c004743f8d4de7e060e107156775a65b23a7f8
MarsIfeanyi <marschalice@gmail.com>
2023-08-03 12:43:32 +0100

    Initial Commit

---
+++ 09-ERC20s/src/ManualToken.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0 <0.9.0;
+
+// Creating the Token manually
+
+contract ManualToken {
+    mapping(address => uint256) private s_balances;
+
+    function name() public pure returns (string memory) {
+        return "Manual Token";
+    }
+
+    function totalSupply() public pure returns (uint256) {
+        return 100 ether; // 100000000000000
+    }
+
+    function decimals() public pure returns (uint8) {
+        return 18;
+    }
+
+    function balanceOf(address _owner) public view returns (uint256) {
+        return s_balances[_owner];
+    }
+
+    function transfer(address _to, uint256 _amount) public {
+        uint256 previousBalances = balanceOf(msg.sender) + balanceOf(_to);
+        s_balances[msg.sender] -= _amount;
+        s_balances[_to] += _amount;
+        require(balanceOf(msg.sender) + balanceOf(_to) == previousBalances);
+    }
+}
