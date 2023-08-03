254595dbe029918550d24dd971c9f9e1a17f41fd
MarsIfeanyi <marschalice@gmail.com>
2023-08-03 08:59:17 +0100

    Initial Commit

---
+++ 08-SmartContract_Lottery/test/integration/InteractionsTest.t.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0 <0.9.0;
+import {Test, console} from "forge-std/Test.sol";
+
+contract InteractionsTest is Test {
+    // TODO: Finish the InteractionsTest and all other tests.
+}
